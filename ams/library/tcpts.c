59cad625bf9805a1efbec22f3304af10efcbfd81
chengzhycn <chengzhycn@gmail.com>
2016-10-07 14:32:17 +0800

    issue#3 update ams/library/tcpts.c(unfinished issue#9)

--- ams/library/tcpts.c
+++ ams/library/tcpts.c
@@ -204,6 +204,8 @@ int	tcpAmsInit(AmsInterface *tsif, char *epspec)
 {
 	unsigned short		portNbr;
 	unsigned int		ipAddress;
+	unsigned char	hostAddr[sizeof(struct in6_addr)];
+	int 	domain;
 	TcptsSap		*sap;
 	socklen_t		buflen;
 	char			endpointNameText[32];
@@ -216,7 +218,7 @@ int	tcpAmsInit(AmsInterface *tsif, char *epspec)
 		epspec = NULL;	/*	Force default selection.	*/
 	}
 
-	parseSocketSpec(epspec, &portNbr, &ipAddress);
+	domain = parseSocketSpec(epspec, &portNbr, hostAddr);
 	if (ipAddress == 0)
 	{
 		if ((ipAddress = getAddressOfHost()) == 0)
